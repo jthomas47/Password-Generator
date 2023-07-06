80c38f9866560be6d5cd87daa6baa3e9edb15c76
jack thomas <jacks@DESKTOP-DP6S8U4.(none)>
2023-07-05 21:46:13 -0500

    added length prompt

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var length = prompt("must be 8 characters and no more than 128 characters");
+  if (length < 8) {
+    alert("password must be at least 8 characters");
+  } else if (length > 128) {
+    alert("password must be less than 128 characters"); 
+  } else if (lenght == "") {
+    alert("choose a value");
+  }
+  
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -15,3 +26,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
